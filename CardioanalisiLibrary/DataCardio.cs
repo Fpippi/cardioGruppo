4c67b95654f9577460edb216d5ecc15144c2621d
Francesco Pippi <anakinpippi321@gmail.com>
2019-04-10 20:15:50 +0200

    ho creato un metodo che controlli se hai inserito lettere o che vada sotto a zero e che ti calcoli la frequenza cardiaca neccesssaria per fare un buon allenamento

--- CardioanalisiLibrary/DataCardio.cs
+++ CardioanalisiLibrary/DataCardio.cs
@@ -8,5 +8,34 @@ namespace CardioanalisiLibrary
 {
     public class DataCardio
     {
+        public static string frequenzaMax_min(string eta,ref string min)
+        {
+            string risultato = "";
+
+            try
+            {
+               int tmp= Convert.ToInt32(eta);
+                if (tmp<=0)
+                {
+                    risultato = "attenzione non puoi inserire un eta uguale a 0 o inferiore";
+                }
+                else
+                {
+                    tmp = 220 - tmp;
+
+                    min = Convert.ToString(tmp * 0.7);
+                    risultato = Convert.ToString(tmp * 0.9);
+                }
+                
+
+            }
+            catch
+            {
+                risultato = "non puoi inserire delle lettere";
+            }
+
+
+            return risultato;
+        }
     }
 }
--- DataCardio.Test/UnitTest1.cs
+++ DataCardio.Test/UnitTest1.cs
@@ -7,12 +7,27 @@ namespace DataCardio.Test
     public class UnitTest1
     {
         [DataTestMethod]
-        [DataRow("w","non puoi inserire delle lettere")]
-        [DataRow(10,189,147)]
-        public void TestMethod1()
+        [DataRow("w","non puoi inserire delle lettere",null)]
+        [DataRow("10","189","147")]
+        [DataRow("0", "attenzione non puoi inserire un eta uguale a 0 o inferiore", null)]
+        public void TestMethod1(string eta,string max,string min)
         {
+            string min2 = "";
+            string max2 = CardioanalisiLibrary.DataCardio.frequenzaMax_min(eta, ref min2);
 
-
+            if(max2== "non puoi inserire delle lettere")
+            {
+                Assert.AreEqual(max, max2);
+            }
+            else if(max2== "attenzione non puoi inserire un eta uguale a 0 o inferiore")
+            {
+                Assert.AreEqual(max, max2);
+            }
+            else
+            {
+                Assert.AreEqual(min, min2);
+                Assert.AreEqual(max, max2);
+            }
 
         }
     }
